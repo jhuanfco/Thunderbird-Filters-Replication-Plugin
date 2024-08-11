c3841bd70d42cdb6a6b7b74a9f62d89fb3a26b02
jhuanfco <jhuanfco@gmail.com>
2024-08-11 19:40:46 +0200

    Reestructuración de ficheros de la api

--- api/implementation.js
+++ api/implementation.js
@@ -1,5 +1,5 @@
 var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
-var xulAppInfo = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo);
+//var xulAppInfo = Components.classes["@mozilla.org/xre/app-info;1"].getService(Components.interfaces.nsIXULAppInfo);
 
 var filterManagerApi = class extends ExtensionCommon.ExtensionAPI {
     getAPI(context) {
@@ -8,7 +8,8 @@ var filterManagerApi = class extends ExtensionCommon.ExtensionAPI {
         async filterManager() {
           let recentWindow = Services.wm.getMostRecentWindow("mail:3pane");
           if (recentWindow) {
-            recentWindow.MsgFilters();
+            let filterWindows = recentWindow.MsgFilters();
+            return filterWindows;
           }
         },
       },
