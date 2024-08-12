842d64c5216f5c121b6c87574c4eb7c809d4c729
jhuanfco <jhuanfco@gmail.com>
2024-08-12 12:15:23 +0200

    Acceso a todas las cuentas y acceso a sus características mediante la ventana de filtros.

--- api/implementation.js
+++ api/implementation.js
@@ -9,9 +9,9 @@ var filterManagerApi = class extends ExtensionCommon.ExtensionAPI {
           try {
             let recentWindow = Services.wm.getMostRecentWindow("mail:3pane");
             if (recentWindow) {
-              console.log("Abriendo nueva ventana de filtros");
+ //             console.log("Abriendo nueva ventana de filtros");
               recentWindow.MsgFilters();
-              console.log("Ventana de filtros debería estar abierta");
+ //             console.log("Ventana de filtros debería estar abierta");
               return recentWindow;
             }
           } catch (error) {
@@ -22,24 +22,38 @@ var filterManagerApi = class extends ExtensionCommon.ExtensionAPI {
         },
 
         async getSelectedAccount() {
-          console.log('Iniciando getSelectedAccount');
+//          console.log('Iniciando getSelectedAccount');
           try {
             let mainWindow = await this.filterManager();
             if (!mainWindow) {
               console.log('No se pudo obtener la ventana principal');
               return null;
             }
-            console.log('Ventana principal obtenida');
+//            console.log('Ventana principal obtenida');
 
             let accountManager = mainWindow.MailServices.accounts;
             let accounts = accountManager.accounts;
+            console.log('NUMERO DE CUENTAS:', accounts.length);
+            console.log('accountManager:', accountManager);
+//           console.log('accounts:', accounts);
 
             if (accounts.length > 0) {
-              let defaultAccount = accountManager.defaultAccount;
-              console.log('Cuenta seleccionada:', defaultAccount.key);
+              let numeroCuentas = -1;
+              do{
+              numeroCuentas++;
+              let selectedAccount = accounts[numeroCuentas];
+              console.log('CONTADOR:', numeroCuentas);
+              console.log('Cuenta seleccionada:', selectedAccount);
+              console.log('Cuenta seleccionada:', selectedAccount.key);
+              console.log('1 4:', selectedAccount.incomingServer.prettyName);
+              console.log('1 5:', selectedAccount.incomingServer.localPath.path);
+//              console.log('1 1:', selectedAccount.defaultIdentity.archiveFolder);
+              } while (numeroCuentas < accounts.length-1)
+
+              let selectedAccount = accounts[1];
               return {
-                key: defaultAccount.key,
-                name: defaultAccount.incomingServer.prettyName
+                key: selectedAccount.key,
+                name: selectedAccount.incomingServer.prettyName
               };
             }
 
