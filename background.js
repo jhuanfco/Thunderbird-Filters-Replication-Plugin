5aeda6ef7840eaa4de332523857896d69e99f962
jhuanfco <jhuanfco@gmail.com>
2024-08-22 18:17:33 +0200

    Cambio de bowser a messenger

--- background.js
+++ background.js
@@ -2,12 +2,12 @@
 //console.log("Inicio de la extensión Thunderbird Filter Copier. ");
 
 // Añade un listener para el evento de clic en el botón de la extensión
-browser.browserAction.onClicked.addListener(() => {
+messenger.browserAction.onClicked.addListener(() => {
     // Registra un mensaje en la consola cuando se hace clic en el botón
-     console.log("SOY background.js --filterCopyApi--abriendo popup");
+     console.log("SOY background.js --filterCopyApi-messenger-popup-abriendo popup");
 
     // Crea y abre una ventana emergente con el contenido de popup.html
-    browser.windows.create({
+    messenger.windows.create({
         url: "popup/popup.html",  // URL del archivo HTML de la ventana emergente
         type: "popup",            // Tipo de ventana: una ventana emergente
         width: 800,               // Ancho de la ventana en píxeles
@@ -19,12 +19,12 @@ browser.browserAction.onClicked.addListener(() => {
 /*
 
 Event Listener:
-   Se añade un event listener al botón de la extensión (browser.browserAction) para el evento 'onClicked'.
+   Se añade un event listener al botón de la extensión (messenger.browserAction) para el evento 'onClicked'.
    Esto significa que la función proporcionada se ejecutará cada vez que el usuario haga clic en el 
    icono de la extensión en la barra de herramientas de Thunderbird.
 
 Acción del clic:
-   Se crea una nueva ventana utilizando browser.windows.create().
+   Se crea una nueva ventana utilizando messenger.windows.create().
 
 Este script  maneja la interacción inicial del usuario (hacer clic en el icono de la extensión) y abre la interfaz principal de la extensión 
 (la ventana emergente). La ventana emergente contendrá la interfaz de usuario principal para la funcionalidad de copia de filtros entre cuentas.
